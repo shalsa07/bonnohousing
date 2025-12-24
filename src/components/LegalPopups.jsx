b74002cbb960ea9ee4bd5068e6c1ec746440614b
Victor Chelemu <victorchelemu@gmail.com>
2025-12-24 22:29:46 +0200

    feat: Improve legal popup UI by adjusting wrapper layout, content spacing, overlay opacity, and close button position.

--- src/components/LegalPopups.jsx
+++ src/components/LegalPopups.jsx
@@ -12,13 +12,13 @@ import { settings } from '@/libs/settings'
 export default function LegalPopups() {
     const {siteState,siteDispatch}=useSiteContext()
   return (
-    (siteState.disclaimerPopup || siteState.termsConditionsPopup || siteState.privacyPolicyPopup || siteState.copyrightNoticePopup) && <div className='legal-popup flex fixed top-0 left-0 right-0 bottom-0 z-[100] justify-center items-center bg-black bg-opacity-50 w-full h-full overflow-y-auto'>
+    (siteState.disclaimerPopup || siteState.termsConditionsPopup || siteState.privacyPolicyPopup || siteState.copyrightNoticePopup) && <div className='legal-popup flex fixed top-0 left-0 right-0 bottom-0 z-[100] justify-center items-center bg-black/65 bg-opacity-50 w-full h-full overflow-y-auto'>
         <LegalPageWrapperPopup>
             {siteState.disclaimerPopup && <DisclaimerComponent/>}
             {siteState.termsConditionsPopup && <TermsConditionsComponent/>}
             {siteState.privacyPolicyPopup && <PrivacyPolicyComponent/>}
             {siteState.copyrightNoticePopup && <CopyrightNoticeComponent/>}
-            <button className={`flex fixed bottom-2 left-0 right-0 mx-auto w-lg h-12 mt-10 items-center rounded-full text-gray-100 uppercase justify-center ${settings.bonnoBlue}`} onClick={()=>siteDispatch({type:ACTIONS_SITE.CLOSE_ALL_POPUPS})}>close</button>
+            <button className={`flex fixed bottom-4 left-0 right-0 mx-auto w-lg h-12 mt-10 items-center rounded-full text-gray-100 uppercase justify-center ${settings.bonnoBlue}`} onClick={()=>siteDispatch({type:ACTIONS_SITE.CLOSE_ALL_POPUPS})}>close</button>
         </LegalPageWrapperPopup>
     </div>)
 }
